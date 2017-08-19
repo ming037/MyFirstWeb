2bef8db387bc7cb44101d9f0b835fe44b1336c00
HWI <ming037@naver.com>
2017-08-19 22:59:28 +0900

    Part3. using templates

--- tutorial/accounts/views.py
+++ tutorial/accounts/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Home page!')
+    return render(request, 'accounts/login.html')   #HttpResponse('Home page!')
