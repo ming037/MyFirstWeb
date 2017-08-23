016e330ff12dec3052a360bc4f0f1d2659498311
HWI <ming037@naver.com>
2017-08-23 14:16:40 +0900

    Part8. LoginPage_1

--- tutorial/accounts/urls.py
+++ tutorial/accounts/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
-from . import views 
+from . import views
+from django.contrib.auth.views import login
 urlpatterns = [
-    url(r'^$', views.home), 
+    url(r'^$', views.home),
+    url(r'^login/$', login, {'template_name':'accounts/login.html'})
+    #두 번째 인자는 view, 마지막 인자는 render용 템플릿
 ]
