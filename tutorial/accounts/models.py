ad729306914c81954c2824627c6e7c2b2fed01f6
HWI <ming037@naver.com>
2017-08-23 16:05:42 +0900

    Part11. Make userProfile model on admin page

--- tutorial/accounts/models.py
+++ tutorial/accounts/models.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User) # add database model
+    description = models.CharField(max_length=100, default='')
+    city = models.CharField(max_length=100, default='')
+    website = models.URLField(default='')
+    phone = models.IntegerField(default=0)
 # Create your models here.
