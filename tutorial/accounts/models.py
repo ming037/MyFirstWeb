fcfe4138c1dc347ed5a9a3bbdfbf031a954e4df3
HWI <ming037@naver.com>
2017-08-23 16:53:41 +0900

    Part12. Signal to trigger code

--- tutorial/accounts/models.py
+++ tutorial/accounts/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User) # add database model
@@ -9,4 +10,11 @@ class UserProfile(models.Model):
     city = models.CharField(max_length=100, default='')
     website = models.URLField(default='')
     phone = models.IntegerField(default=0)
-# Create your models here.
+
+def create_profile(sender, **kwargs):
+    if kwargs['created']: #connet에서 sender=User의 User가 create되면 아래 코드 실행
+        user_Profile = UserProfile.objects.create(user=kwargs['instance'])
+        # kwargs['instance']는 sender=User의 오브젝트이다.
+
+#장고 오프젝트를 저장하고 post_save 시그널 후에 코드가 동작함.
+post_save.connect(create_profile, sender=User)
