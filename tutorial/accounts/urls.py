60e236421af5c2739b38ca0c145225ea7b969877
HWI <ming037@naver.com>
2017-08-24 18:49:56 +0900

    Part14. Redirect home page

--- tutorial/accounts/urls.py
+++ tutorial/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 urlpatterns = [
     url(r'^$', views.home),
-    url(r'^login/$', login, {'template_name':'accounts/login.html'})
+    url(r'^login/$', login, {'template_name':'accounts/login.html'}),
+    url(r'^logout/$', logout, {'template_name':'accounts/logout.html'})
     #두 번째 인자는 view, 마지막 인자는 render용 템플릿
 ]
