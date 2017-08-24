647e0f6ba080add8107f25c473330372c315b334
HWI <ming037@naver.com>
2017-08-24 19:05:20 +0900

    Part15. Make register proccess

--- tutorial/accounts/urls.py
+++ tutorial/accounts/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth.views import login, logout
 urlpatterns = [
     url(r'^$', views.home),
     url(r'^login/$', login, {'template_name':'accounts/login.html'}),
-    url(r'^logout/$', logout, {'template_name':'accounts/logout.html'})
+    url(r'^logout/$', logout, {'template_name':'accounts/logout.html'}),
+    url(r'^register/$', views.register, name='register')
     #두 번째 인자는 view, 마지막 인자는 render용 템플릿
 ]
--- tutorial/accounts/views.py
+++ tutorial/accounts/views.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
@@ -11,3 +12,15 @@ def home(request):
     args = {'myName':name, 'numbers':numbers}
     return render(request, 'accounts/home.html', args)
     #return render(request, 'accounts/login.html', args)   #HttpResponse('Home page!')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()  # 유저 생성해서 DB에 넣음
+            return redirect('/account')
+    else: #GET일 경우
+        form = UserCreationForm()
+
+        args ={'form':form}
+        return render(request, 'accounts/reg_form.html', args)
