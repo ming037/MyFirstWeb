e0622a2c96d87e555d1eb24701ab9e9f573163b4
HWI <ming037@naver.com>
2017-08-27 17:11:45 +0900

    Pull from remote

--- tutorial/accounts/urls.py
+++ tutorial/accounts/urls.py
@@ -1,3 +1,4 @@
+#-*-coding: utf-8 -*-
 from django.conf.urls import url
 from . import views
 from django.contrib.auth.views import login, logout
@@ -5,6 +6,8 @@ urlpatterns = [
     url(r'^$', views.home),
     url(r'^login/$', login, {'template_name':'accounts/login.html'}),
     url(r'^logout/$', logout, {'template_name':'accounts/logout.html'}),
-    url(r'^register/$', views.register, name='register')
+    url(r'^register/$', views.register, name='register'),
+    url(r'^profile/$', views.view_profile, name='view_profile'),
+    url(r'^profile/edit/$', views.edit_profile, name='edit_profile')
     #두 번째 인자는 view, 마지막 인자는 render용 템플릿
 ]
--- tutorial/accounts/views.py
+++ tutorial/accounts/views.py
@@ -2,7 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from accounts.forms import RegistrationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm
 
 # Create your views here.
 def home(request):
@@ -15,12 +17,27 @@ def home(request):
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()  # 유저 생성해서 DB에 넣음
             return redirect('/account')
     else: #GET일 경우
-        form = UserCreationForm()
+        form = RegistrationForm()
 
         args ={'form':form}
         return render(request, 'accounts/reg_form.html', args)
+
+def view_profile(request):
+    args = {'user':request.user}
+    return render(request, 'accounts/profile.html', args)
+
+def edit_profile(request):
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('/account/profile')
+    else: # Get
+        form = UserChangeForm(instance=request.user)
+        args ={'form': form}
+        return render(request, 'accounts/edit_profile.html', args)
