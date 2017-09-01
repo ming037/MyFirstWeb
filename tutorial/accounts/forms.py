0b2a07fc44538a9a5f2f2f58812747dc0c22c3c0
HWI <ming037@naver.com>
2017-09-01 13:37:58 +0900

    Part19. Edit Personal Information

--- tutorial/accounts/forms.py
+++ tutorial/accounts/forms.py
@@ -1,20 +1,21 @@
+# -*- coding: utf-8 -*-
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 class RegistrationForm(UserCreationForm): # inherit
     email = forms.EmailField(required=True)
 
     class Meta: # contain Metadata
         model = User
-        fields = {
+        fields = (
             'username',
             'first_name',
             'last_name',
             'email',
             'password1',
             'password2'
-         }
+         )
     def save(self, commit=True): # override
         user = super(RegistrationForm, self).save(commit=False)
         user.first_name = self.cleaned_data['first_name']
@@ -24,3 +25,13 @@ class RegistrationForm(UserCreationForm): # inherit
         if commit:
             user.save() #sql database saving
         return user
+
+class EditProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'first_name',
+            'last_name',
+            'password') #포함하고 싶은 필드, password는 꼭 포함하여야 한다.
+        exclude = () #제거하고 싶은 필드 , 둘 중 아무거나 사용해도 상관없음
--- tutorial/accounts/views.py
+++ tutorial/accounts/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render,redirect
-from accounts.forms import RegistrationForm
+from accounts.forms import RegistrationForm, EditProfileForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserChangeForm
 
@@ -33,11 +33,11 @@ def view_profile(request):
 
 def edit_profile(request):
     if request.method == 'POST':
-        form = UserChangeForm(request.POST, instance=request.user)
+        form = EditProfileForm(request.POST, instance=request.user) #UserChangeForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
             return redirect('/account/profile')
     else: # Get
-        form = UserChangeForm(instance=request.user)
+        form = EditProfileForm(instance=request.user) #UserChangeForm(instance=request.user)
         args ={'form': form}
         return render(request, 'accounts/edit_profile.html', args)
