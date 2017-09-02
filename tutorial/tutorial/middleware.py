45cc63f1ba5c4c09bd7832eeb1439fd836fab14e
HAN HWI <ming037@naver.com>
2017-09-02 21:17:02 +0900

    Part28. Refactoring middleware.py

--- tutorial/tutorial/middleware.py
+++ tutorial/tutorial/middleware.py
@@ -4,6 +4,7 @@
 import re #regular expressions
 from django.conf import settings
 from django.shortcuts import redirect
+from django.contrib.auth import logout
 
 EXEMPT_URLS = [re.compile(settings.LOGIN_URL.lstrip('/'))]
 if hasattr(settings, 'LOGIN_EXEMPT_URLS'):
@@ -21,6 +22,18 @@ class LoginRequiredMiddleware:
         assert hasattr(request, 'user') #user가 있는지 doublecheck
         path = request.path_info.lstrip('/')
 
-        if not request.user.is_authenticated():
-            if not any(url.match(path) for url in EXEMPT_URLS):
-                return redirect(settings.LOGIN_URL)
+        # if not request.user.is_authenticated(): # logout 하면 특정 페이지들 접근 못하도록.
+        #     if not any(url.match(path) for url in EXEMPT_URLS):
+        #         return redirect(settings.LOGIN_URL)
+
+        url_is_exempt = any(url.match(path) for url in EXEMPT_URLS)
+
+        if path == 'account/logout/':  #이게 없으면 로그아웃이 안 됨.
+            logout(request)
+
+        if request.user.is_authenticated() and url_is_exempt:  #login 이미 했으면 login이나 register에 접근할 수 없다.
+            return redirect(settings.LOGIN_REDIRECT_URL)
+        if request.user.is_authenticated() or url_is_exempt: #login 되어있는데 url이 exempt 아닌 경우, 반대 경우도 해당
+            return None
+        else: #login 안 되었을 경우
+            return redirect(settings.LOGIN_URL)
