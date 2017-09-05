2a125d6976382ea8393d74bb5822549df8d526e9
HWI <ming037@naver.com>
2017-09-05 13:27:08 +0900

    Part33,34. fix password_reset page

--- tutorial/accounts/urls.py
+++ tutorial/accounts/urls.py
@@ -18,7 +18,10 @@ urlpatterns = [
       name='reset_password'),
 
     url(r'^reset-password/done/$', password_reset_done, {'template_name':'accounts/reset_password_done.html'},name='password_reset_done'),
-    url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm, name='password_reset_confirm'),
-    url(r'^reset-password/complete/$', password_reset_complete, name='password_reset_complete'),
+    url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm,
+    {'template_name':'accounts/reset_password_confirm.html',
+    'post_reset_redirect':'accounts:password_reset_complete'},name='password_reset_confirm'),
+
+    url(r'^reset-password/complete/$', password_reset_complete, {'template_name':'accounts/reset_password_complete.html'},name='password_reset_complete'),
     #두 번째 인자는 view, 마지막 인자는 render용 템플릿
 ]
