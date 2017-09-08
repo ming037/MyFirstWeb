81971f07ee1906a8e7448cf7a488b72dadd89811
HWI <ming037@naver.com>
2017-09-08 13:53:17 +0900

    Part44

--- tutorial/home/urls.py
+++ tutorial/home/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
 from home.views import HomeView #from home import views
 urlpatterns = [
-    url(r'^$', HomeView.as_view(), name='home') #url(r'^$', views.home, name='home')
+    url(r'^$', HomeView.as_view(), name='home') #url(r'^$', views.home, name='home')  class based view
 ]
