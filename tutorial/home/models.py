6adbf028f266f3fdba50b4b8a04815e89d4cf927
HWI <ming037@naver.com>
2017-09-09 17:22:10 +0900

    Part48. get data from django model and put into HTML template

--- tutorial/home/models.py
+++ tutorial/home/models.py
@@ -4,3 +4,4 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     post = models.CharField(max_length=500)
     user = models.ForeignKey(User)
+    date = models.DateTimeField(auto_now=True)
--- tutorial/home/views.py
+++ tutorial/home/views.py
@@ -1,20 +1,25 @@
 from django.views.generic import TemplateView #for class bases views
 from home.forms import HomeForm
 from django.shortcuts import render, redirect
+from home.models import Post
+
 class HomeView(TemplateView):
     template_name = 'home/home.html'
 
     def get(self, request):
         form = HomeForm()
-        return render(request, self.template_name, {'form':form})
+        posts = Post.objects.all()
+
+        args = {'form':form, 'posts':posts}
+        return render(request, self.template_name,args)
 
     def post(self, request):
         form = HomeForm(request.POST)  #with the data
         if form.is_valid():
             post =form.save(commit=False) #post데이터를 전부 저장한다.
             post.user = request.user
-            post.save()
-            
+            post.save() #django shell로 데이터 확인할 수 있다.
+
             text = form.cleaned_data['post']   #cleaned_data 쓰게 되면 sql인젝션 같은 걸 거를 수 있다. 위에 있는 post는 forms.py에 정의한 form이다.
             form = HomeForm() #submit 하면 editbox의 데이터가 사라진다.
             return redirect('home:home') #이렇게 하면 페이지 초기화. 데이터 전송 같은거 하고 난 후 쓰면 됨.
