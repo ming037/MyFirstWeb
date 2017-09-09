d84d33c50c8f379db7958172b634c776bfbe0fd3
HWI <ming037@naver.com>
2017-09-09 17:03:44 +0900

    Part47. Save data through form using django model form

--- tutorial/home/forms.py
+++ tutorial/home/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from home.models import Post
+class HomeForm(forms.ModelForm): #forms.form 대신에 ModelForm을 쓰는 이유는. ModelForm은 models.py와 연결되어 있다. 이걸 bound라고 함.
+    post = forms.CharField()  #html에서 할 수 있지만 form으로 이런식으로 하는게 더 안전하다.
 
-class HomeForm(forms.Form):
-    post = forms.CharField()  #html에서 할 수 있지만 form으로 이런식으로 하는게 더 안전하다. 
+    class Meta:
+        model = Post
+        fields = ('post',) #콤마 안하면 tutple이 아니기 때문에 주의하여야 한다.
--- tutorial/home/views.py
+++ tutorial/home/views.py
@@ -11,6 +11,10 @@ class HomeView(TemplateView):
     def post(self, request):
         form = HomeForm(request.POST)  #with the data
         if form.is_valid():
+            post =form.save(commit=False) #post데이터를 전부 저장한다.
+            post.user = request.user
+            post.save()
+            
             text = form.cleaned_data['post']   #cleaned_data 쓰게 되면 sql인젝션 같은 걸 거를 수 있다. 위에 있는 post는 forms.py에 정의한 form이다.
             form = HomeForm() #submit 하면 editbox의 데이터가 사라진다.
             return redirect('home:home') #이렇게 하면 페이지 초기화. 데이터 전송 같은거 하고 난 후 쓰면 됨.
